95ba5a10a2484960f8d36358c1fe7d0a812fcfd1
Abourass <Abourassa@AssetVal.com>
2023-11-01 21:43:03 -0600

    Update title and remove SoilCalculator component

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,11 @@
-import SoilCalculator from '~/components/SoilCalculator.jsx';
 
 export default function Home() {
   return (
     <main class="text-center mx-auto p-4">
       <h1 class="max-6-xs text-6xl text-eggplant-900 dark:text-night-400 font-thin uppercase mb-4">
-        Modern Mix Calculator
+        TheModern.Farm
       </h1>
 
-      <SoilCalculator />
     </main>
   );
 }
