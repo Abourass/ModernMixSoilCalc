176e8aeea3a1e1698d030d14ed8c30a00190fd20
Abourass <Abourassa@AssetVal.com>
2023-11-06 22:40:28 -0700

    Update Home page with new header and sample article

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,10 +2,13 @@
 export default function Home() {
   return (
     <main class="text-center mx-auto p-4">
-      <h1 class="max-6-xs text-6xl text-eggplant-900 dark:text-night-400 font-thin uppercase mb-4">
+      <h1 class="text-6xl text-eggplant-900 dark:text-night-400 mb-4 font-family-posterama-thin">
         TheModern.Farm
       </h1>
 
+      <article class="max-w-screen-xl mx-auto dark:bg-cinder-900 dark:text-slate-300 rounded-lg shadow-md overflow-hidden">
+        <span>Sample</span>
+      </article>
     </main>
   );
 }
