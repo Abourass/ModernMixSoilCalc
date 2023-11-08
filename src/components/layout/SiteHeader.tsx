717de9e579788ef59d5fd30a5f53c17b49445542
Abourass <Abourassa@AssetVal.com>
2023-11-07 22:29:35 -0700

    Add motion to SiteHeader title.

--- src/components/layout/SiteHeader.tsx
+++ src/components/layout/SiteHeader.tsx
@@ -1,8 +1,9 @@
-export default function SiteHeader() {
+import { Motion } from '@motionone/solid';
 
-  return (<>
-    <h1 class="text-6xl text-eggplant-200 dark:text-night-400 font-family-posterama-thin center">
+export default function SiteHeader() {
+  return (<Motion.div>
+    <h1 class="text-header text-eggplant-200 dark:text-night-400 font-family-posterama-thin center mt-1 subpixel-antialiased">
       TheModern.Farm
     </h1>
-  </>)
+  </Motion.div>)
 }
