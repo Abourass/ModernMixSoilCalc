dbee23ee3ea41c00730bc1410b7c65a63478f76b
Abourass <Abourassa@AssetVal.com>
2023-11-07 22:30:07 -0700

    Update calculator title font size

--- src/routes/calculator.tsx
+++ src/routes/calculator.tsx
@@ -3,7 +3,7 @@ import SoilCalculator from '~/components/SoilCalculator.jsx';
 export default function Calculator() {
   return (
     <main class="text-center mx-auto p-4">
-      <h1 class="max-6-xs text-6xl text-eggplant-900 dark:text-night-400 font-thin uppercase mb-4">
+      <h1 class="max-6-xs text-4xl text-eggplant-900 dark:text-night-400 font-thin uppercase mb-4">
         Modern Mix Calculator
       </h1>
 
