e54347a82e6ffe3c5f4ca4a3595c3c160c9104de
Abourass <Abourassa@AssetVal.com>
2023-11-07 22:29:26 -0700

    Update styles in Home component

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,7 +2,7 @@
 export default function Home() {
   return (
     <main class="text-center mx-auto p-4">
-      <article class="max-w-screen-xl mx-auto dark:bg-cinder-900 dark:text-slate-300 rounded-lg shadow-md overflow-hidden">
+      <article class="max-w-screen-xl mx-auto bg-eggplant-200 dark:bg-night-900 dark:text-gray-200 rounded-md shadow-md overflow-hidden">
         <span>Sample</span>
       </article>
     </main>
