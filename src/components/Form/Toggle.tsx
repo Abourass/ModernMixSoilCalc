94e3e297de28a472126d730efaf7809b52ada330
Abourass <Abourassa@AssetVal.com>
2023-11-07 22:30:04 -0700

    Update Toggle component with Motion animation and color options

--- src/components/Form/Toggle.tsx
+++ src/components/Form/Toggle.tsx
@@ -1,31 +1,65 @@
-import { createSignal, splitProps } from 'solid-js';
+import { Motion } from '@motionone/solid';
+import { splitProps } from 'solid-js';
 
-type FontIntensity = '100' | '200' | '300' | '400' | '500' | '600' | '700' | '800' | '900';
+type Intensity = '100' | '200' | '300' | '400' | '500' | '600' | '700' | '800' | '900';
+type Colors = 'gray' | 'red' | 'yellow' | 'green' | 'blue' | 'indigo' | 'purple' | 'pink' | 'eggplant' | 'slate' | 'white'
 
 interface ToggleProps {
   label: string,
   value: string,
-  color?: 'red' | 'blue' | 'green' | 'yellow' | 'indigo' | 'purple' | 'pink' | 'gray' | 'white' | 'black',
+  color?: Colors,
   oninput?: (event: InputEvent & { currentTarget: HTMLInputElement; target: HTMLInputElement; }) => void;
+  /** @default {false} */
   disabled?: boolean
   classList?: string,
+  /** @default {'gray'} */
+  bgColor?: Colors,
+  /** @default {'200'} */
+  bgIntensity?: Intensity,
+  /** @default {'gray'} */
+  darkBgColor?: Colors,
+  /** @default {'700'} */
+  darkBgIntensity?: Intensity,
+  /** @default {'blue'} */
+  checkedBgColor?: Colors,
+  /** @default {'600'} */
+  checkedBgIntensity?: Intensity,
+  /** @default {'blue'} */
+  ringColor?: Colors,
+  /** @default {'300'} */
+  ringIntensity?: Intensity,
+  /** @default {'blue'} */
+  darkRingColor?: Colors,
+  /** @default {'800'} */
+  darkRingIntensity?: Intensity,
+  /** @default {'4'} */
+  ringSize?: '0' | '1' | '2' | '3' | '4' | '8' | 'inset',
   labelClassList?: string,
-  labelTextColor?: string,
-  labelTextIntensity?: FontIntensity,
-  labelDarkTextColor?: string,
-  labelDarkTextIntensity?: FontIntensity,
+  /** @default {'gray'} */
+  labelTextColor?: Colors,
+  /** @default {'900'} */
+  labelTextIntensity?: Intensity,
+  /** @default {'gray'} */
+  labelDarkTextColor?: Colors,
+  /** @default {'300'} */
+  labelDarkTextIntensity?: Intensity,
+  /** @default {'white'} */
+  dotColor?: Colors,
+  /** @default {'gray'} */
+  dotBorderColor?: Colors,
+  /** @default {'300'} */
+  dotBorderIntensity?: Intensity,
+  /** @default {false} */
   checked?: boolean,
+
 }
 
 export default function Toggle(props: ToggleProps) {
   // R is required props, O is optional props
   const [r, o] = splitProps(props, ['label', 'value']);
 
-  // eslint-disable-next-line solid/reactivity
-  const [color] = createSignal<NonNullable<ToggleProps['color']>>(o.color as ToggleProps['color'] ?? 'blue');
-
   return (
-    <label class="relative inline-flex items-center mr-5 cursor-pointer">
+    <Motion.label class="relative inline-flex items-center cursor-pointer">
       <input
         type="checkbox"
         value={r.value}
@@ -36,25 +70,24 @@ export default function Toggle(props: ToggleProps) {
       />
       <div
         class={`
-        w-11 h-6
-        bg-gray-200
-        rounded-full
+        w-11 h-6 rounded-full
+        bg-${o.bgColor ?? 'gray'}-${o.bgIntensity ?? '200'}
+        dark:bg-${o.darkBgColor ?? 'gray'}-${o.darkBgIntensity ?? '700'}
         peer
-        peer-focus:ring-4
-        peer-focus:ring-${color()}-300
-        dark:peer-focus:ring-${color()}-800
-        dark:bg-gray-700
+        peer-focus:ring-${o.ringSize ?? '4'}
+        peer-focus:ring-${o.ringColor ?? 'blue'}-${o.ringIntensity ?? '300'}
+        dark:peer-focus:ring-${o.darkRingColor ?? 'blue'}-${o.darkRingIntensity ?? '800'}
         peer-checked:after:translate-x-full
         peer-checked:after:border-white after:content-['']
         after:absolute after:top-0.5 after:left-[2px]
-        after:bg-white
-        after:border-gray-300
+        after:bg-${o.dotColor ?? 'white'}
+        after:border-${o.dotBorderColor ?? 'gray'}-${o.dotBorderIntensity ?? '300'}
         after:border
         after:rounded-full
         after:h-5 after:w-5
         after:transition-all
-        dark:border-gray-600
-        peer-checked:bg-${color()}-600
+        dark:border-grey-600
+        peer-checked:bg-${o.checkedBgColor ?? 'blue'}-${o.checkedBgIntensity ?? '600'}
         ${o.classList ? o.classList : ""}`}
       />
       <span class={`
@@ -65,6 +98,6 @@ export default function Toggle(props: ToggleProps) {
         dark:text-${o.labelDarkTextColor ?? 'gray'}-${o.labelDarkTextIntensity ?? 300}
         ${o.labelClassList ? o.labelClassList : ''}
       `}>{r.label}</span>
-    </label>
+    </Motion.label>
   )
 }
