6d1e816b09116f7afd47ed940fbca3842b0be02f
Abourass <Abourassa@AssetVal.com>
2023-11-13 22:17:21 -0700

    Refactor SiteHeader component to remove Motion dependency.

--- src/components/layout/SiteHeader.tsx
+++ src/components/layout/SiteHeader.tsx
@@ -1,13 +1,14 @@
-import { Motion } from '@motionone/solid';
 import { splitProps } from 'solid-js';
 
 export default function SiteHeader(props?: { class?: string }) {
   // R is required props, O is optional props
   const [R] = splitProps(props ?? {}, ['class']);
 
-  return (<Motion.div class={R.class ?? ''}>
-    <h1 class="text-header text-eggplant-200 dark:text-night-400 font-family-posterama-thin center mt-1 subpixel-antialiased">
-      TheModern.Farm
-    </h1>
-  </Motion.div>)
+  return (
+    <div class={R.class ?? ''}>
+      <h1 class="text-header text-eggplant-200 dark:text-night-400 font-family-posterama-thin center mt-1 subpixel-antialiased">
+        TheModern.Farm
+      </h1>
+    </div>
+  )
 }
